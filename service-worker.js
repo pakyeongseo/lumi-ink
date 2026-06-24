400e689f5828a70ba105f4762e8dde24b5ad385a
pakyeongseo <uzrilucy@gmail.com>
2026-06-25 08:25:02 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,22 +2,14 @@
    업데이트를 배포할 때는 아래 CACHE 버전 숫자만 올리면
    기존 캐시가 정리되고 새 파일로 갱신됩니다. (예: v1 -> v2) */
 const CACHE_PREFIX = "ink-memo-";
-const CACHE = "ink-memo-v63.32-music-player";
+const CACHE = "ink-memo-v63.33-speech-bubbles";
 
 const ASSETS = [
   "./",
   "./index.html",
   "./Lumi_Ink_Manual_1.html",
-  "./Lumi_Ink_v63.23_Release_Notes.md",
-  "./Lumi_Ink_v63.24_Release_Notes.md",
-  "./Lumi_Ink_v63.25_Release_Notes.md",
-  "./Lumi_Ink_v63.26_Release_Notes.md",
-  "./Lumi_Ink_v63.27_Release_Notes.md",
-  "./Lumi_Ink_v63.28_Release_Notes.md",
-  "./Lumi_Ink_v63.29_Release_Notes.md",
-  "./Lumi_Ink_v63.30_Release_Notes.md",
-  "./Lumi_Ink_v63.31_Release_Notes.md",
   "./Lumi_Ink_v63.32_Release_Notes.md",
+  "./Lumi_Ink_v63.33_Release_Notes.md",
   "./app.js",
   "./assets-icons.js",
   "./assets-frames.js",
